7fc94aedb9c2a7e18688a42bd649c14c00ff9a1b
Luiz Felipe Frez <luiz.frez.santos@escola.pr.gov.br>
2026-09-22 09:05:18 -0300

    okkk02

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const botaoTema = document.querySelector("#botao-tema");
-        const temaSalvo = localStorage.getItem("tema");
+document.addEventListener("DOMContentLoaded", () => {
+    const botaoTema = document.querySelector("#botao-tema");
+    const temaSalvo = localStorage.getItem("tema");
 
-        if (temaSalvo === "escuro") {
-            document.body.classList.add("tema-escuro");
+    if (temaSalvo === "escuro") {
+        document.body.classList.add("tema-escuro");
+        botaoTema.textContent = "☀ Tema";
+    } else {
+        botaoTema.textContent = "🌙 Tema";
+    }
+
+    botaoTema.addEventListener("click", function () {
+        document.body.classList.toggle("tema-escuro");
+
+        if (document.body.classList.contains("tema-escuro")) {
             botaoTema.textContent = "☀ Tema";
+            localStorage.setItem("tema", "escuro");
+        } else {
+            botaoTema.textContent = "🌙 Tema";
+            localStorage.setItem("tema", "claro");
         }
-
-        botaoTema.addEventListener("click", function () {
-            document.body.classList.toggle("tema-escuro");
-
-            if (document.body.classList.contains("tema-escuro")) {
-                botaoTema.textContent = "☀ Tema";
-                localStorage.setItem("tema", "escuro");
-            } else {
-                botaoTema.textContent = "🌙 Tema";
-                localStorage.setItem("tema", "claro");
-            }
-        });
+    });
+});
